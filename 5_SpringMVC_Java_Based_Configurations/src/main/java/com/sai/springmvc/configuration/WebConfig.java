5248a2bd3a9d3d40fcc7b5b5398d712c103c4e4c
ssais <ssais@sai>
2025-08-26 17:10:15 +0530

    initial commit

---
+++ 5_SpringMVC_Java_Based_Configurations/src/main/java/com/sai/springmvc/configuration/WebConfig.java
@@ -0,0 +1,24 @@
+package com.sai.springmvc.configuration;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebConfig implements WebApplicationInitializer  {
+
+public void onStartup(ServletContext servletContext){
+
+AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+context.register(JavaSpringMVCConfiguration.class);
+
+DispatcherServlet dispatcher = new DispatcherServlet(context);
+
+javax.servlet.ServletRegistration.Dynamic servlet =  servletContext.addServlet("dispatcher",dispatcher);
+
+servlet.setLoadOnStartup(1);
+servlet.addMapping("/");
+}
+}
